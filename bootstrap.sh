95af2850f3ad73f4cf30b340d12cdd364b51321d
ikirt <mike@triki.ca>
2016-11-06 00:40:47 -0400

    created lists for both apt and pip package installs.

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,65 +19,36 @@ echo """
 echo """
         Updating Distro...
     """
-
 apt-get update -y
 
 
 echo """
-        Installing necessary tools...
+        Installing necessary tools from apt
     """
+apt_app_list="nbtscan python-scapy tcpdump nmap python-nmap python-bluez \
+              smbclient samba samba_common_bin unzip python-dev python3-dev \
+              libpcap-dev ppp xprobe2 sg3-utils netdiscover macchanger onesixtyone \
+              nikto hydra john bridge-utils w3af-console ettercap-text-only cryptcat \
+              ike-scan"
+apt-get install $apt_app_list -y
 
-apt-get install nbtscan -y
-apt-get install python-scapy -y
-apt-get install tcpdump -y
-apt-get install nmap -y 
-apt-get install python-nmap -y 
-apt-get install python-bluez -y
-apt-get install smbclient -y
-apt-get install samba -y
-apt-get install samba_common_bin -y
-apt-get install unzip -y
-sudo apt-get install python-dev
-sudo apt-get install python3-dev
-
-pip install optparse-pretty
-pip install netaddr 
-pip install ipaddress 
-pip install subprocess32
-pip install pypcap
-apt-get install libpcap-dev -y
-
-apt-get install ppp -y
-apt-get install xprobe2 -y
-apt-get install sg3-utils -y
-apt-get install netdiscover -y
-apt-get install macchanger -y
+echo """
+        Installing necessary tools from pip
+    """
+py_app_list="optparse-pretty netaddr ipaddress subprocess32 pypcap"
+pip install $py_app_list
 
 wget http://seclists.org/nmap-dev/2016/q2/att-201/clamav-exec.nse -O /usr/share/nmap/scripts/clamav-exec.nse
 wget https://labs.portcullis.co.uk/download/enum4linux-0.8.9.tar.gz -O $root_dir/Tools/
-tar -zxvf enum4linux-0.8.9.tar.gz
-mv enum4linux-0.8.9 enum4linux
-
-
 
 cd $root_dir/Tools
-
+tar -zxvf enum4linux-0.8.9.tar.gz
+mv enum4linux-0.8.9 enum4linux
 
 echo """
         Cloning some awesome repos...
     """
-
 git clone https://github.com/DanMcInerney/net-creds.git
-
-apt-get install onesixtyone -y
-apt-get install nikto -y
-apt-get install hydra -y
-apt-get install john -y
-apt-get install bridge-utils -y
-apt-get install w3af-console -y
-apt-get install ettercap-text-only -y
-apt-get install cryptcat -y
-apt-get install ike-scan -y
 git clone https://github.com/sqlmapproject/sqlmap.git
 git clone https://github.com/CoreSecurity/impacket.git
 git clone https://github.com/samratashok/nishang.git
@@ -86,8 +57,6 @@ git clone https://github.com/PowerShellMafia/PowerSploit.git
 git clone https://github.com/offensive-security/exploit-database.git
 wget https://download.sysinternals.com/files/SysinternalsSuite.zip
 unzip SysinternalsSuite.zip -d sysinternals/
-
-
 
 echo """
         All done ready to rock...
