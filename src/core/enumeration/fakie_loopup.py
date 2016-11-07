4e5594c3eb0c8b48253619ef9f71474eb08a8469
ikirt <mike@triki.ca>
2016-11-06 23:48:03 -0500

    added new module for reverse ip lookup

--- src/core/enumeration/fakie_loopup.py
+++ src/core/enumeration/fakie_loopup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python2.7
-#Description:
-__author__ = ''
-__date__ = ''
+#Description: reverse lookup of hosts found on network
+
+import socket
+
+def fakie_ip_lookup(ip):
+    if os.path.isfile('../Results/mailcreds'):
+        print
+        bcolors.WARNING + "[!] Mail Creds Results File Exists. Previous Results will be overwritten\n " + bcolors.ENDC
+    with open('../Results/mailcreds', 'w') as mailcreds:
+    hostInfo=socket.gethostbyaddr(ip)    #result i wamt hostInfo[2]
